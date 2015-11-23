de6cd61fa14eb6876164800052b0c76d847e4930
Juliana Moraes <Juliana Moraes>
2015-11-23 11:03:03 -0300

    adicionando mongoose e tendo que mudar o arquivo \disapp\node_modules\mongoose\node_modules\mongodb\node_modules\bson\ext\index.html que apontava para o caminho ..\build\Release\bson quando deveria apontar para ..\browser_build\bson e dava erro fiquei horas nesse erro, tive que instalar python, setar python rodar npm install -g node-gyp

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var  http = require('http');
 var app = require('./config/express')();
+require('./config/database.js')('mongodb://localhost/disapp');
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express Server escutando na porta ' +
